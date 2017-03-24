16bc5dadfa9bb517c46dab188cf670faf98468fd
JP Marra <jp.marra@me.com>
2017-03-24 16:12:08 -0400

    Delete info.js

--- credentials/info.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  host: 'us-cdbr-iron-east-03.cleardb.net',
-  user: 'bf9c4d3d080f04',
-  password: '4e609f6c',
-  database: 'heroku_ae04e88237e79a3'
-}
-
-//mysql://bf9c4d3d080f04:4e609f6c@us-cdbr-iron-east-03.cleardb.net/heroku_ae04e88237e79a3?reconnect=true
