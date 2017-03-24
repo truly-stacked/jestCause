48f5c3aa4014cbf24eac8c3279c4d7668ee2b5ac
JP Marra <jp.marra@me.com>
2017-03-24 12:08:42 -0400

    added credentials

---
+++ credentials/info.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'b76b500028d7c7',
+  password: 'a935b6b9',
+  database: 'heroku_e233e93b207bbdf'
+}
+
+//mysql://b76b500028d7c7:a935b6b9@us-cdbr-iron-east-03.cleardb.net/heroku_e233e93b207bbdf?reconnect=true
